8866321c778b46ee5b8ec2f4f84616d2d315760a
uestc-zb <96124033+uestc-zb@users.noreply.github.com>
2024-04-24 16:29:36 +0000

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,37 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let vec: Vec<&str> = s.split(',').collect();
+        if vec.len() < 2 {
+            return Person::default();
+        }
+
+        let name = vec[0].to_string();
+        if name.is_empty() {
+            return Person::default();
+        }
+        let age = vec[1].parse::<usize>();
+        match age {
+            Ok(age) => {
+                if vec.len() > 2 {
+                    Person::default()
+                } else {
+                    Person {
+                        name,
+                        age,
+                    }
+                }
+            },
+            
+            Err(_) => Person::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+
+        let vec: Vec<&str> = s.split(',').collect();
+        if vec.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = vec[0].to_string();
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age_result = vec[1].parse::<usize>();
+        let age = match age_result {
+            Ok(a) => a,
+            Err(e) => return Err(ParsePersonError::ParseInt(e)),
+        };
+
+        Ok(Person { name, age })
     }
 }
 
